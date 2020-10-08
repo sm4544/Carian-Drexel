0ae3c63308a202df346d6f08cf901c7177f301e4
srini <sn862@drexel.edu>
2020-10-07 20:34:58 -0400

    included TDD framework (jest engyme) #15

--- Frontend/Carian/App.js
+++ Frontend/Carian/App.js
@@ -1,4 +1,26 @@
-import React, { Component } from "react";
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+{/* import React, { Component } from "react";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 
@@ -10,4 +32,4 @@ const App = createStackNavigator({
   Register: Register,
   Dashboard: Dashboard,
 });
-export default createAppContainer(App);
+export default createAppContainer(App);*/}
