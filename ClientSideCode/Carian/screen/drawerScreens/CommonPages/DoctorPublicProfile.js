efe91fee9793676dbbb0c8810acba7cbeb4ea882
srini <sn862@drexel.edu>
2021-03-13 09:54:22 -0500

    formatting main code

--- ClientSideCode/Carian/screen/drawerScreens/CommonPages/DoctorPublicProfile.js
+++ ClientSideCode/Carian/screen/drawerScreens/CommonPages/DoctorPublicProfile.js
@@ -1,14 +1,14 @@
 import ValidationComponent from 'react-native-form-validator';
 import React from 'react';
-import {View, Text, TouchableOpacity, Image, ScrollView} from 'react-native';
+import { View, Text, TouchableOpacity, Image, ScrollView } from 'react-native';
 import styles from '../../../styles/DoctorProfileStyles';
 import SpecialityCard from '../Cards/SpecialityCard';
 import moment from 'moment';
 import ReviewCard from '../Cards/ReviewCard';
-import {Table, Row, Rows} from 'react-native-table-component';
+import { Table, Row, Rows } from 'react-native-table-component';
 import CalendarStrip from 'react-native-calendar-strip';
-import {SliderBox} from 'react-native-image-slider-box';
-import {getAvailableSlots, getDoctorDetails} from '../../services/hospitalService';
+import { SliderBox } from 'react-native-image-slider-box';
+import { getAvailableSlots, getDoctorDetails } from '../../services/hospitalService';
 const image = {
   uri:
     'https://st4.depositphotos.com/10313122/20884/i/1600/depositphotos_208847620-stock-photo-studio-shot-of-young-beautiful.jpg',
@@ -19,79 +19,23 @@ export default class DoctorPublicProfile extends ValidationComponent {
     super(props);
     (this.state = {
       doctor: {},
-      hospital:{},
-      customerCount:1000,
+      hospital: {},
+      customerCount: 1000,
       selectedDate: moment().format('MM/DD/YYYY'),
       selectedTime: '',
       hospitalImageList: [],
       specialistCarddata: [
-        {image: image, name: 'Family physicians'},
-        {image: image, name: 'Pediatricians'},
-        {image: image, name: 'Geriatric doctors'},
-        {image: image, name: 'Allergists'},
-        {image: image, name: 'Dermatologists'},
-        {image: image, name: 'Ophthalmologists'},
-        {image: image, name: 'Infectious disease doctors'},
-        {image: image, name: 'Obstetrician/gynecologists'},
-        {image: image, name: 'Cardiologists'},
-        {image: image, name: 'Endocrinologists'},
-        {image: image, name: 'Gastroenterologists'},
-        {image: image, name: 'Nephrologists'},
-        {image: image, name: 'Urologists'},
-        {image: image, name: 'Pulmonologists'},
-        {image: image, name: 'Otolaryngologists'},
-        {image: image, name: 'Neurologists'},
-        {image: image, name: 'Psychiatrists'},
-        {image: image, name: 'Oncologists'},
-        {image: image, name: 'Radiologists'},
-        {image: image, name: 'General surgeons'},
-        {image: image, name: 'Orthopedic surgeons'},
-        {image: image, name: 'Cardiac surgeons'},
-        {image: image, name: 'Anesthesiologists'},
-        {image: image, name: 'Rheumatologists'},
+        { image: image, name: 'Family physicians' },
+        { image: image, name: 'Pediatricians' },
+        { image: image, name: 'Geriatric doctors' },
+
       ],
       doctorReviews: [],
-      services:[],
+      services: [],
       headerSlots: ['Days', 'Morning', 'Afternoon', 'Evening', 'Night'],
       workingHours: [
         [
           'Mon',
-          '10:00AM-12:00AM',
-          '01:00PM-05:00PM',
-          '06:00PM-10:00PM',
-          '10:00PM-07:00AM',
-        ],
-        ['Tue', '10:00AM-12:00AM', '-', '06:00PM-10:00PM', '-'],
-        [
-          'Wed',
-          '10:00AM-12:00AM',
-          '01:00PM-05:00PM',
-          '06:00PM-10:00PM',
-          '10:00PM-07:00AM',
-        ],
-        [
-          'Thu',
-          '10:00AM-12:00AM',
-          '01:00PM-05:00PM',
-          '06:00PM-10:00PM',
-          '10:00PM-07:00AM',
-        ],
-        [
-          'Fri',
-          '10:00AM-12:00AM',
-          '01:00PM-05:00PM',
-          '06:00PM-10:00PM',
-          '10:00PM-07:00AM',
-        ],
-        [
-          'Sat',
-          '10:00AM-12:00AM',
-          '01:00PM-05:00PM',
-          '06:00PM-10:00PM',
-          '10:00PM-07:00AM',
-        ],
-        [
-          'Sun',
           '10:00AM-12:00AM',
           '01:00PM-05:00PM',
           '06:00PM-10:00PM',
@@ -103,35 +47,31 @@ export default class DoctorPublicProfile extends ValidationComponent {
     this.onPressingContinueButton = this.onPressingContinueButton.bind(this);
   }
   displaySlots = (date) => {
-    ;
-    this.setState({selectedDate: date.format('MM/DD/YYYY')});
+    this.setState({ selectedDate: date.format('MM/DD/YYYY') });
   };
   onpressTime = (time) => {
-    
-    this.setState({selectedTime: time});
+    this.setState({ selectedTime: time });
   };
   onPressingContinueButton = () => {
     if (this.state.selectedDate == '' || this.state.selectedTime == '') {
-      this.setState({error: true});
+      this.setState({ error: true });
     } else {
       this.props.navigation.navigate('PatientsScreen', {
         date: this.state.selectedDate,
         time: this.state.selectedTime,
         doctor: this.state.doctor,
-        hospital:this.state.hospital,
+        hospital: this.state.hospital,
         profileId: global.profileId,
       });
     }
   };
   getAvailableSlots = (doctorID) => {
-    
     const body = JSON.stringify({
       doctorID: doctorID,
-      days:30
+      days: 30
     });
     getAvailableSlots(body)
-
-      .then((res) => {        
+      .then((res) => {
         var list1 = [];
         let day = moment().add(1, 'days').format("YYYY-MM-DD")
         for (i = 0; i < res[day].length; i++) {
@@ -141,70 +81,61 @@ export default class DoctorPublicProfile extends ValidationComponent {
           });
         }
         this.setState({ slots: list1, calen: res })
-
       })
       .catch((error) => {
         console.log(error);
       });
   };
-  getDoctorDetails = (id) =>{
+
+  getDoctorDetails = (id) => {
     const body = JSON.stringify({
       doctor_id: id,
     });
     getDoctorDetails(body)
-
       .then((res) => {
-                
         const doc = {
           id: res.doctor.id,
-          name : res.doctor.name,
-          image : image,
+          name: res.doctor.name,
+          image: image,
           highestDegree: res.doctor.highest_qualification,
-          specialization:res.doctor.specialization,
+          specialization: res.doctor.specialization,
           overAllExperience: res.doctor.overall_work_experience,
-          totalNoOfReviews:150,
+          totalNoOfReviews: 150,
           avgRating: 4,
           doctor_fee: res.doctor.doctor_fee,
-          
+        }
+        const h = {
+          hospital_name: res.hospital.name,
+          area: res.hospital.area,
+          city: res.hospital.city,
+          state: res.hospital.state,
+          pincode: res.hospital.pincode,
+          streatAddline1: res.hospital.addressine1,
+          streatAddline2: res.hospital.addressine2,
+          avgRating: 4,
+          totalNoOfReviews: 120
         }
 
-        const h ={
-          hospital_name:res.hospital.name,
-          area:res.hospital.area,
-          city:res.hospital.city,
-          state:res.hospital.state,
-          pincode:res.hospital.pincode,
-          streatAddline1:res.hospital.addressine1,
-          streatAddline2:res.hospital.addressine2,
-          avgRating:4,
-          totalNoOfReviews:120
-
+        var list3 = []
+        for (i = 0; i < 1; i++) {
+          list3.push({
+            id: i,
+            image: image,
+            name: doc.specialization,
+          });
         }
-
-        var list3 =[]
-        for (i = 0; i < 1; i++) { 
-        list3.push({
-          id:i,
-          image: image,
-          name: doc.specialization,
-        });
-      }
-
-      const hours = res.working_hours[0];
-     
-      const wHours = [["Monday", hours.Monday.split('-')[0], hours.Monday.split('-')[1]],
-       ["Tuesday", hours.Tuesday.split('-')[0], hours.Tuesday.split('-')[1]],
-       ["Wednesday", hours.Wednesday.split('-')[0], hours.Wednesday.split('-')[1]],
-       ["Thursday", hours.Thursday.split('-')[0], hours.Thursday.split('-')[1]],
-       ["Friday", hours.Friday.split('-')[0], hours.Friday.split('-')[1]],
-       ["Saturday", hours.Saturday.split('-')[0], hours.Saturday.split('-')[1]],
-       ["Sunday", hours.Sunday.split('-')[0], hours.Sunday.split('-')[1]]]
-      
-
-        this.setState({ hospitalImageList: res.hospitalImages, services: res.services, doctor: doc, 
-          doctorReviews: res.reviews, hospital: h, workingHours: wHours, specialistCarddata: list3})
-
-        
+        const hours = res.working_hours[0];
+        const wHours = [["Monday", hours.Monday.split('-')[0], hours.Monday.split('-')[1]],
+        ["Tuesday", hours.Tuesday.split('-')[0], hours.Tuesday.split('-')[1]],
+        ["Wednesday", hours.Wednesday.split('-')[0], hours.Wednesday.split('-')[1]],
+        ["Thursday", hours.Thursday.split('-')[0], hours.Thursday.split('-')[1]],
+        ["Friday", hours.Friday.split('-')[0], hours.Friday.split('-')[1]],
+        ["Saturday", hours.Saturday.split('-')[0], hours.Saturday.split('-')[1]],
+        ["Sunday", hours.Sunday.split('-')[0], hours.Sunday.split('-')[1]]]
+        this.setState({
+          hospitalImageList: res.hospitalImages, services: res.services, doctor: doc,
+          doctorReviews: res.reviews, hospital: h, workingHours: wHours, specialistCarddata: list3
+        })
       })
       .catch((error) => {
         console.log(error);
@@ -214,38 +145,36 @@ export default class DoctorPublicProfile extends ValidationComponent {
   componentDidMount() {
     this.getAvailableSlots(this.props.navigation.state.params.id);
     this.getDoctorDetails(this.props.navigation.state.params.id)
-    }
-
-  
+  }
 
   render() {
-  
+
     let customDatesStyles = [];
     let startDate = moment();
     let endDate = moment(startDate).add(30, 'days');
-    let datesWhitelist = [{start: startDate, end: endDate}];
-    
+    let datesWhitelist = [{ start: startDate, end: endDate }];
+
     let slots = [
-      {id: 0, time: '09:30 AM'},
-      {id: 1, time: '10:00 AM'},
-      {id: 2, time: '10:30 AM'},
-      {id: 3, time: '11:00 AM'},
-      {id: 4, time: '11:30 AM'},
-      {id: 5, time: '12:00 AM'},
-      {id: 12, time: '12:30 AM'},
-      {id: 6, time: '01:00 PM'},
-      {id: 7, time: '01:30 PM'},
-      {id: 8, time: '02:00 PM'},
-      {id: 9, time: '02:30 PM'},
-      {id: 10, time: '03:00 PM'},
-      {id: 11, time: '03:30 PM'},
+      { id: 0, time: '09:30 AM' },
+      { id: 1, time: '10:00 AM' },
+      { id: 2, time: '10:30 AM' },
+      { id: 3, time: '11:00 AM' },
+      { id: 4, time: '11:30 AM' },
+      { id: 5, time: '12:00 AM' },
+      { id: 12, time: '12:30 AM' },
+      { id: 6, time: '01:00 PM' },
+      { id: 7, time: '01:30 PM' },
+      { id: 8, time: '02:00 PM' },
+      { id: 9, time: '02:30 PM' },
+      { id: 10, time: '03:00 PM' },
+      { id: 11, time: '03:30 PM' },
     ];
 
     let services = [
-      {id: 0, name: 'treatment A'},
-      {id: 1, name: 'treatment B'},
-      {id: 2, name: 'treatment C'},
-      {id: 3, name: 'treatment D'},
+      { id: 0, name: 'treatment A' },
+      { id: 1, name: 'treatment B' },
+      { id: 2, name: 'treatment C' },
+      { id: 3, name: 'treatment D' },
     ];
 
     for (let i = 0; i < 40; i++) {
@@ -316,7 +245,7 @@ export default class DoctorPublicProfile extends ValidationComponent {
           ) : null}
           <View style={styles.slotsView}>
             {slots.map((item) => (
-              <View key={item.id} style={{flexBasis: '25%'}}>
+              <View key={item.id} style={{ flexBasis: '25%' }}>
                 <TouchableOpacity
                   onPress={() => this.onpressTime(item.time)}
                   key={item.id}
@@ -336,7 +265,7 @@ export default class DoctorPublicProfile extends ValidationComponent {
               </Text>
 
 
-              <View style={{flexDirection: 'row'}}>
+              <View style={{ flexDirection: 'row' }}>
                 <TouchableOpacity style={styles.button}>
                   <Text style={styles.buttonText}>Website</Text>
                 </TouchableOpacity>
@@ -390,38 +319,29 @@ export default class DoctorPublicProfile extends ValidationComponent {
               </View>
             </View>
           </View>
-
           <Text style={styles.sectionTitle}>Location</Text>
-
           <View style={styles.horizontalLine} />
-
           <Text style={styles.sectionTitle}>Clinic Photos</Text>
-
-         
-
           <SliderBox
             images={this.state.hospitalImageList}
             sliderBoxHeight={180}
             dotColor="#FFEE58"
             inactiveDotColor="#90A4AE"
           />
-
           <View style={styles.horizontalLine} />
           <Text style={styles.sectionTitle}>Specialization</Text>
           <View style={styles.imagesRowSetUp}>
-            <View style={{flexBasis: '50%'}}>
+            <View style={{ flexBasis: '50%' }}>
               <SpecialityCard
                 data={this.state.specialistCarddata[0]}
-                style={{backgroundColor: 'white'}}></SpecialityCard>
+                style={{ backgroundColor: 'white' }}></SpecialityCard>
             </View>
           </View>
-
           <View style={styles.horizontalLine} />
           <Text style={styles.sectionTitle}>Services</Text>
-
           <View style={styles.slotsView}>
             {this.state.services.map((item) => (
-              <View key={item.id} style={{flexBasis: '31%'}}>
+              <View key={item.id} style={{ flexBasis: '31%' }}>
                 <TouchableOpacity
                   key={item.id}
                   disabled={true}
@@ -431,7 +351,6 @@ export default class DoctorPublicProfile extends ValidationComponent {
               </View>
             ))}
           </View>
-
           <View style={styles.horizontalLine} />
           <Text style={styles.sectionTitle}>Working Hours</Text>
           <Table style={styles.tableStyle}>
