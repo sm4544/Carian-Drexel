a69a52d27e943262a73579ce5aead64cbcd38084
AnooshaDhoppalapudi <adhoppalapu2@dxc.com>
2021-03-15 16:15:13 -0400

    Calendar Changes

--- ClientSideCode/Carian/__tests__/screen/DoctorCalendarScreen-test.js
+++ ClientSideCode/Carian/__tests__/screen/DoctorCalendarScreen-test.js
@@ -9,6 +9,10 @@ import { expect } from 'chai';
 import sinon from 'sinon';
 import { Dropdown } from 'react-native-material-dropdown-v2';
 import DoctorCalendarScreen from '../../screen/drawerScreens/DoctorCalendarScreen';
+import DropDownPicker from 'react-native-dropdown-picker';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import styles2 from '../../screen/drawerScreens/DoctorCalendarScreen';
+import styles from '../../styles/DoctorProfileStyles';
 const navigation = {
   navigate: jest.fn(),
   state: {
@@ -43,6 +47,7 @@ global.expect = expect;
 global.sinon = sinon;
 global.shallow = shallow;
 
+
 describe('<DoctorCalendarScreen/>', () => {
   beforeEach(function () {
     spyon = sinon.spy(navigation, 'navigate');
@@ -55,41 +60,48 @@ describe('<DoctorCalendarScreen/>', () => {
     navigation.navigate.restore();
   });
 
-  it('should have ScrollView', () => {
-    expect(wrapper.type()).to.equal(ScrollView);
-  });
 
   it('should have View', () => {
-    expect(wrapper.find(View)).to.have.length(3);
+    expect(wrapper.find(View)).to.have.length(30);
   });
 
   it('should have  text component', () => {
-    expect(wrapper.find(Text)).to.have.length(7);
+    expect(wrapper.find(Text)).to.have.length(15);
   });
 
-  it('timeSlot should not be empty on load', () => {
-    expect(wrapper.state('timeSlots')).to.be.an('array').that.is.empty;
-  });
 
   it('Initially Dropdown length should be zero', () => {
-    expect(wrapper.find(Dropdown)).to.have.length(0);
+    expect(wrapper.find(DropDownPicker)).to.have.length(14);
   });
-
-  it('day should be Monday', async () => {
-    wrapper.instance().handleClickMonday('Monday');
+  it('should display patient appointment details', () => {
+    expect(wrapper.contains(<TouchableOpacity onPress={this.onPressSubmit} activeOpacity={0.7} style={styles2.button} >
+   
+      <Text style={styles.buttonText}> Submit </Text>
+   
+       </TouchableOpacity>)).to.equal(false);
   });
-
-
-  it('day should be Tuesday', async () => {
-    wrapper.instance().handleClickTuesday('Tuesday');
+  it('should display text Monday ', () => {
+    expect(wrapper.contains(<Text  style={{ fontWeight: 'bold', fontSize : 20}} >Monday</Text>)).to.equal(true);
   });
-
-  it('day should be Wednesday', async () => {
-    wrapper.instance().handleClickWednesday('Wednesday');
+  it('should display text Tuesday ', () => {
+    expect(wrapper.contains(<Text  style={{ fontWeight: 'bold', fontSize : 20}} >Tuesday</Text>)).to.equal(true);
   });
-
-  it('Before handleClick', async () => {
-    wrapper.instance().handleClick();
-    expect(wrapper.find({ show: true })).to.have.length(0);
+  it('should display text Wednesday', () => {
+    expect(wrapper.contains(<Text  style={{ fontWeight: 'bold', fontSize : 20}} >Wednesday</Text>)).to.equal(true);
   });
+  it('should display text Thursday ', () => {
+    expect(wrapper.contains(<Text  style={{ fontWeight: 'bold', fontSize : 20}} >Thursday</Text>)).to.equal(true);
+  });
+  it('should display text Friday ', () => {
+    expect(wrapper.contains(<Text  style={{ fontWeight: 'bold', fontSize : 20}} >Friday</Text>)).to.equal(true);
+  });
+  it('should display text Saturday ', () => {
+    expect(wrapper.contains(<Text  style={{ fontWeight: 'bold', fontSize : 20}} >Saturday</Text>)).to.equal(true);
+  });
+  it('should display text Sunday ', () => {
+    expect(wrapper.contains(<Text  style={{ fontWeight: 'bold', fontSize : 20}} >Sunday</Text>)).to.equal(true);
+  });
+  
+
+  
 });
--- ClientSideCode/Carian/screen/services/doctorAppointmentService.js
+++ ClientSideCode/Carian/screen/services/doctorAppointmentService.js
@@ -12,3 +12,15 @@ export const getAppointmentDetails = async (body) => {
     });
     return await res.json();   
 };
+export const postDoctorSchedule = async (body) => {
+    console.log(body);
+    const res = await fetch(URL + 'DoctorWorkingHours/', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+       body:body,
+    });
+    return await res.json();   
+};
